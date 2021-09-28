4d9788dc9cb545936d31cd4cbad564ecb16a343a
rjoshi-deq <rjoshi@deqode.com>
2021-09-28 23:57:22 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from "uuid";
+export const data = [
+  {
+    name: "Title 1",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+    artist: "Vachak A",
+    audio: "/audio/4.mp4",
+    color: ["#205950", "#2ab3bf"],
+    id: uuidv4(),
+    active: true,
+  },
+  {
+    name: "Title 2",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+    artist: "Vachak B",
+    audio: "/audio/3.mpeg",
+    color: ["#EF8EA9", "#ab417f"],
+    id: uuidv4(),
+    active: false,
+  },
+  {
+    name: "Title 3",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+    artist: "Vachak C",
+    audio: "/audio/2.mpeg",
+    color: ["#CD607D", "#c94043"],
+    id: uuidv4(),
+    active: false,
+  },
+  {
+    name: "Title 4",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+    artist: "Vachak D",
+    audio: "/audio/1.mpeg",
+    color: ["#EF8EA9", "#ab417f"],
+    id: uuidv4(),
+    active: false,
+  },
+  {
+    name: "Reflection",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+    artist: "Swørn",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+    color: ["#CD607D", "#c94043"],
+    id: uuidv4(),
+    active: false,
+  },
+  {
+    name: "Under the City Stars",
+    cover:
+      "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+    artist: "Aso, Middle School, Aviino",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+    color: ["#205950", "#2ab3bf"],
+    id: uuidv4(),
+    active: false,
+  },
+  //ADD MORE HERE
+];
